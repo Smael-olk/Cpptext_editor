c24ba3c9458c713481898c0b07dcc211242dbd4b
Smael_olk <oulkhir.ismael@gmail.com>
2025-01-19 23:05:13 +0100

    Win32 Text editor

---
+++ tex_editor.cpp
@@ -0,0 +1,160 @@
+#include <windows.h>
+#include <commdlg.h>
+
+// Global variables
+HWND hEdit;
+
+// Function prototypes
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+void OpenFile(HWND hwnd);
+void SaveFile(HWND hwnd);
+
+// Entry point
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Register the window class
+    const char* CLASS_NAME = "TextEditorClass";
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles
+        CLASS_NAME,                     // Window class
+        "Text Editor",                  // Window title
+        WS_OVERLAPPEDWINDOW,            // Window style
+        CW_USEDEFAULT, CW_USEDEFAULT,   // Position
+        800, 600,                       // Size
+        NULL,                           // Parent window
+        NULL,                           // Menu
+        hInstance,                      // Instance handle
+        NULL                            // Additional application data
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    // Create the Edit control
+    hEdit = CreateWindowEx(
+        0, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL,
+        0, 0, 800, 600, hwnd, NULL, hInstance, NULL
+    );
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
+
+    // Message loop
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+// Window procedure
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CREATE: {
+            // Add a menu bar
+            HMENU hMenu = CreateMenu();
+            HMENU hFileMenu = CreateMenu();
+            AppendMenu(hFileMenu, MF_STRING, 1, "Open");
+            AppendMenu(hFileMenu, MF_STRING, 2, "Save");
+            AppendMenu(hFileMenu, MF_STRING, 3, "Exit");
+            AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, "File");
+            SetMenu(hwnd, hMenu);
+            break;
+        }
+        case WM_COMMAND: {
+            switch (LOWORD(wParam)) {
+                case 1: // Open
+                    OpenFile(hwnd);
+                    break;
+                case 2: // Save
+                    SaveFile(hwnd);
+                    break;
+                case 3: // Exit
+                    PostQuitMessage(0);
+                    break;
+            }
+            break;
+        }
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
+// Open file function
+void OpenFile(HWND hwnd) {
+    OPENFILENAME ofn;
+    char szFile[260] = {0};
+
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = hwnd;
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    if (GetOpenFileName(&ofn)) {
+        HANDLE hFile = CreateFile(ofn.lpstrFile, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hFile != INVALID_HANDLE_VALUE) {
+            DWORD dwFileSize = GetFileSize(hFile, NULL);
+            char* pszFileContent = (char*)malloc(dwFileSize + 1);
+            if (pszFileContent) {
+                ReadFile(hFile, pszFileContent, dwFileSize, NULL, NULL);
+                pszFileContent[dwFileSize] = '\0';
+                SetWindowText(hEdit, pszFileContent);
+                free(pszFileContent);
+            }
+            CloseHandle(hFile);
+        }
+    }
+}
+
+// Save file function
+void SaveFile(HWND hwnd) {
+    OPENFILENAME ofn;
+    char szFile[260] = {0};
+
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = hwnd;
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_OVERWRITEPROMPT;
+
+    if (GetSaveFileName(&ofn)) {
+        HANDLE hFile = CreateFile(ofn.lpstrFile, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hFile != INVALID_HANDLE_VALUE) {
+            DWORD dwTextLength = GetWindowTextLength(hEdit);
+            char* pszText = (char*)malloc(dwTextLength + 1);
+            if (pszText) {
+                GetWindowText(hEdit, pszText, dwTextLength + 1);
+                WriteFile(hFile, pszText, dwTextLength, NULL, NULL);
+                free(pszText);
+            }
+            CloseHandle(hFile);
+        }
+    }
+}
